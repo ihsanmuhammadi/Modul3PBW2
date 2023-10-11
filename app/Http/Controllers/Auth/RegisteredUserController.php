99cb13bb1d1838649483f01722557199e5789238
ihsanmuhammadi <ihsanmiqbal24@gmail.com>
2023-10-11 21:06:47 +0700

    Pengumpulan Tugas Modul 3

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+    // NIM : 6706220123
+    // NAMA : IHSAN MUHAMMAD IQBAL
+    // KELAS : 46-03
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'fullname' => ['required', 'string', 'max:100'],
+            'adress' => ['required', 'string', 'max:1000'],
+            'username' => ['required', 'string', 'max:100'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'birthdate' => ['required', 'date'],
+            'phoneNumber' => ['required', 'string', 'max:20'],
+            'agama' => ['required', 'string', 'max:20'],
+            'gender' => ['required', 'integer', 'max:4']
+        ]);
+
+        $user = User::create([
+            'fullname' => $request->fullname,
+            'adress' => $request->adress,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'birthdate' => $request->birthdate,
+            'phoneNumber' => $request->phoneNumber,
+            'agama' => $request->agama,
+            'gender' => $request->gender
+        ]);
+
+        event(new Registered($user));
+
+        // Auth::login($user);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
---
+++ app/Models/Collection.php
@@ -0,0 +1,21 @@
+<?php
+    // NIM : 6706220123
+    // NAMA : IHSAN MUHAMMAD IQBAL
+    // KELAS : 46-03
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Collection extends Model {
+    use HasFactory;
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'namaKoleksi',
+        'jenisKoleksi',
+        'jumlahKoleksi',
+        'namaPengarang',
+        'tahunTerbit'
+    ];
+}
